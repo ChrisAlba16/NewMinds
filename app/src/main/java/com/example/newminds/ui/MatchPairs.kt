6efc444ed07c39b60e9d10841750e0f37bd22433
Christopher Alba Arcos <al057202@uacam.mx>
2023-02-26 23:58:09 -0600

    Ayudame Dios

--- app/src/main/java/com/example/newminds/ui/MatchPairs.kt
+++ app/src/main/java/com/example/newminds/ui/MatchPairs.kt
@@ -2,118 +2,179 @@ package com.example.newminds.ui
 
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newminds.databinding.ActivityMatchPairsBinding
-import java.lang.Thread.sleep
+import com.example.newminds.utils.Pairs
+import com.example.newminds.utils.Requests
 
 class MatchPairs : AppCompatActivity() {
     private lateinit var binding: ActivityMatchPairsBinding
-
+    private lateinit var pregunta: Pairs
+    private var contador = 0
+    private var b1 = ""
+    private var b2 = ""
+    private var buttonClicked1 = 0
+    private var buttonClicked2 = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMatchPairsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        var leftOne = 2
-        var leftTwo = 2
-        var rightOne = 2
-        var rightTwo = 2
-        var pairOne = 2
-        var pairTwo = 2
-
+        pregunta = Requests.pairs(1,1)[0]
+        if(pregunta.video == "" || pregunta.video == null){
+            binding.duda.visibility = View.INVISIBLE
+        }
+        else{
+            binding.duda.setOnClickListener {
+                val link = Uri.parse(pregunta.video)
+                val i = Intent(Intent.ACTION_VIEW,link)
+                startActivity(i)
+            }
+        }
+        binding.cancelar.setOnClickListener {
+            val intent = Intent(this@MatchPairs, LessonList::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.leftOne.text = pregunta.pairs[0][0]
         binding.leftOne.setOnClickListener {
-            if ((leftOne == 1 && rightTwo == 0) || (leftOne == 0 && rightTwo == 1)) {
-                binding.fondo.setBackgroundColor(Color.RED)
-                sleep(1000)
-                val intent = Intent(this@MatchPairs, MatchPairs::class.java)
-                startActivity(intent)
-                finish()
+            if(b1 != ""){
+                b2 = binding.leftOne.text.toString()
+                buttonClicked2 = binding.leftOne.id
+                if(checkCorrecto(b1,b2,pregunta.pairs)){
+                    val btn1 = findViewById<Button>(buttonClicked1)
+                    val btn2 = findViewById<Button>(buttonClicked2)
+                    btn1.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                    btn2.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                }
+                buttonClicked1 = 0
+                buttonClicked2 = 0
+                b1 = ""
+                b2 = ""
             }
-            leftOne = 1
-            leftTwo = 0
-            rightOne = 0
-            rightTwo = 0
-            binding.leftOne.setTextColor(Color.BLUE)
-            binding.leftTwo.setTextColor(Color.BLACK)
-            if (leftOne == 1 && rightTwo == 1) {
-                pairOne = 1
-                binding.leftOne.isClickable = false
-                binding.rightTwo.isClickable = false
-                binding.leftOne.setTextColor(Color.GREEN)
-                binding.rightTwo.setTextColor(Color.GREEN)
-            }
-
-            if (pairOne == 1 && pairTwo == 1) {
-                val intent = Intent(this@MatchPairs, LessonList::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }
-        binding.leftTwo.setOnClickListener {
-            leftOne = 0
-            leftTwo = 1
-            rightOne = 0
-            rightTwo = 0
-            binding.leftTwo.setTextColor(Color.BLUE)
-            binding.leftOne.setTextColor(Color.BLACK)
-            if (leftTwo == 1 && rightOne == 1) {
-                pairTwo = 1
-                binding.leftTwo.isClickable = false
-                binding.rightOne.isClickable = false
-                binding.leftTwo.setTextColor(Color.GREEN)
-                binding.rightOne.setTextColor(Color.GREEN)
-            }
-
-            if (pairOne == 1 && pairTwo == 1) {
-                val intent = Intent(this@MatchPairs, LessonList::class.java)
-                startActivity(intent)
-                finish()
+            else{
+                b1 = binding.leftOne.text.toString()
+                buttonClicked1 = binding.leftOne.id
             }
         }
 
+        binding.leftTwo.text = pregunta.pairs[1][0]
+        binding.leftOne.setOnClickListener {
+            if(b1 != ""){
+                b2 = binding.leftTwo.text.toString()
+                buttonClicked2 = binding.leftOne.id
+                if(checkCorrecto(b1,b2,pregunta.pairs)){
+                    val btn1 = findViewById<Button>(buttonClicked1)
+                    val btn2 = findViewById<Button>(buttonClicked2)
+                    btn1.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                    btn2.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                }
+                buttonClicked1 = 0
+                buttonClicked2 = 0
+                b1 = ""
+                b2 = ""
+            }
+            else{
+                b1 = binding.leftTwo.text.toString()
+                buttonClicked1 = binding.leftOne.id
+            }
+        }
+
+        binding.rightOne.text = pregunta.pairs[0][1]
         binding.rightOne.setOnClickListener {
-            leftOne = 0
-            leftTwo = 0
-            rightOne = 1
-            rightTwo = 0
-            binding.rightOne.setTextColor(Color.BLUE)
-            binding.rightTwo.setTextColor(Color.BLACK)
-            if (leftTwo == 1 && rightOne == 1) {
-                pairTwo = 1
-                binding.leftTwo.isClickable = false
-                binding.rightOne.isClickable = false
-                binding.leftTwo.setTextColor(Color.GREEN)
-                binding.rightOne.setTextColor(Color.GREEN)
+            if(b1 != ""){
+                b2 = binding.rightOne.text.toString()
+                buttonClicked2 = binding.leftOne.id
+                if(checkCorrecto(b1,b2,pregunta.pairs)){
+                    val btn1 = findViewById<Button>(buttonClicked1)
+                    val btn2 = findViewById<Button>(buttonClicked2)
+                    btn1.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                    btn2.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                }
+                buttonClicked1 = 0
+                buttonClicked2 = 0
+                b1 = ""
+                b2 = ""
             }
-
-            if (pairOne == 1 && pairTwo == 1) {
-                val intent = Intent(this@MatchPairs, LessonList::class.java)
-                startActivity(intent)
-                finish()
+            else{
+                b1 = binding.rightOne.text.toString()
+                buttonClicked1 = binding.leftOne.id
             }
         }
+
+        binding.rightTwo.text = pregunta.pairs[1][1]
         binding.rightTwo.setOnClickListener {
-            leftOne = 0
-            leftTwo = 0
-            rightOne = 0
-            rightTwo = 1
-            binding.rightTwo.setTextColor(Color.BLUE)
-            binding.rightOne.setTextColor(Color.BLACK)
-            if (leftOne == 1 && rightTwo == 1) {
-                pairOne = 1
-                binding.leftOne.isClickable = false
-                binding.rightTwo.isClickable = false
-                binding.leftOne.setTextColor(Color.GREEN)
-                binding.rightTwo.setTextColor(Color.GREEN)
+            if(b1 != ""){
+                b2 = binding.rightTwo.text.toString()
+                buttonClicked2 = binding.leftOne.id
+                if(checkCorrecto(b1,b2,pregunta.pairs)){
+                    val btn1 = findViewById<Button>(buttonClicked1)
+                    val btn2 = findViewById<Button>(buttonClicked2)
+                    btn1.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                    btn2.isClickable = false
+                    btn1.setBackgroundColor(Color.parseColor("#004000"))
+                }
+                buttonClicked1 = 0
+                buttonClicked2 = 0
+                b1 = ""
+                b2 = ""
             }
-
-            if (pairOne == 1 && pairTwo == 1) {
-                val intent = Intent(this@MatchPairs, LessonList::class.java)
-                startActivity(intent)
-                finish()
+            else{
+                b1 = binding.rightTwo.text.toString()
+                buttonClicked1 = binding.leftOne.id
             }
         }
+    }
+    private fun checkCorrecto(a: String, b: String, lista: Array<Array<String>>): Boolean{
+        var correcto = false
+        for(item in lista){
+            correcto = ((a == item[0] && b == item[1]) || (a == item[1] && b == item[0])) || correcto
+        }
+        if(correcto){
+            contador += 1
+            val mensaje = Toast.makeText(binding.root.context,"Bien, sólo te falta una más",Toast.LENGTH_LONG).show()
+            if (contador >= 2){
+                correcto()
+            }
+            return true
+        }
+        else{
+            equivocado()
+            return false
+        }
+    }
+    private fun correcto(){
+        val colorDrawables = arrayOf(
+            ColorDrawable(Color.parseColor("#007f00")),
+            ColorDrawable(Color.WHITE)
+        )
+        val transitionDrawable = TransitionDrawable(colorDrawables)
+        binding.fondo.background = transitionDrawable
+        val mensaje = Toast.makeText(binding.root.context,"¡Muy bien hecho!", Toast.LENGTH_LONG).show()
+        transitionDrawable.startTransition(5000)
+    }
+    private fun equivocado(){
+        val colorDrawables = arrayOf(
+            ColorDrawable(Color.parseColor("#7f0000")),
+            ColorDrawable(Color.WHITE)
+        )
+        val transitionDrawable = TransitionDrawable(colorDrawables)
+        binding.fondo.background = transitionDrawable
+        val mensaje = Toast.makeText(binding.root.context,"Parece que esa no es la respuesta, vuelve a intentar",
+            Toast.LENGTH_LONG).show()
+        transitionDrawable.startTransition(1000)
     }
 }
--- app/src/main/java/com/example/newminds/ui/MultipleChoiceUI.kt
+++ app/src/main/java/com/example/newminds/ui/MultipleChoiceUI.kt
@@ -1,39 +1,64 @@
 package com.example.newminds.ui
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newminds.databinding.ActivityMultiplechoiceBinding
 import com.example.newminds.utils.MultipleChoice
+import com.example.newminds.utils.Requests
+import java.net.URI
 
 class MultipleChoiceUI:AppCompatActivity() {
-    lateinit var binding: ActivityMultiplechoiceBinding
-    lateinit var pregunta: MultipleChoice
+    private lateinit var binding: ActivityMultiplechoiceBinding
+    private lateinit var pregunta: MultipleChoice
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = com.example.newminds.databinding.ActivityMultiplechoiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val respuestas = arrayOf("Una ideología","Una política","Un instituto","Un país")
-        val enlaces = arrayOf("","","","")
-        pregunta = MultipleChoice(1,1,"¿Qué es el comunismo?",respuestas,enlaces,"1","",1)
+        pregunta = Requests.multipleChoice(1,1)[0]
+        if(pregunta.video == "" || pregunta.video == null){
+            binding.duda.visibility = View.INVISIBLE
+        }
+        else{
+            binding.duda.setOnClickListener {
+                val link = Uri.parse(pregunta.video)
+                val i = Intent(Intent.ACTION_VIEW,link)
+                startActivity(i)
+            }
+        }
+        val answer = Integer.parseInt(pregunta.respuesta)
         binding.textView.text = pregunta.texto
         binding.option1.text = pregunta.options[0]
         binding.option2.text = pregunta.options[1]
         binding.option3.text = pregunta.options[2]
         binding.option4.text = pregunta.options[3]
         binding.option1.setOnClickListener {
-            checkCorrecto(1,pregunta.respuesta.toInt())
+            checkCorrecto(1,answer)
         }
         binding.option2.setOnClickListener {
-            checkCorrecto(2,pregunta.respuesta.toInt())
+            checkCorrecto(2,answer)
         }
         binding.option3.setOnClickListener {
-            checkCorrecto(3,pregunta.respuesta.toInt())
+            checkCorrecto(3,answer)
         }
         binding.option4.setOnClickListener {
-            checkCorrecto(4,pregunta.respuesta.toInt())
+            checkCorrecto(4,answer)
+        }
+        binding.cancelar.setOnClickListener {
+            val intent = Intent(this@MultipleChoiceUI, LessonList::class.java)
+            startActivity(intent)
+            finish()
         }
     }
-    fun checkCorrecto(opcion: Int, respuesta: Int){
+    private fun checkCorrecto(opcion: Int, respuesta: Int){
         if(opcion == respuesta){
             correcto()
         }
@@ -41,10 +66,18 @@ class MultipleChoiceUI:AppCompatActivity() {
             equivocado()
         }
     }
-    fun correcto(){
-
+    private fun correcto(){
+        val colorDrawables = arrayOf(ColorDrawable(Color.parseColor("#007f00")),ColorDrawable(Color.WHITE))
+        val transitionDrawable = TransitionDrawable(colorDrawables)
+        binding.fondo.background = transitionDrawable
+        val mensaje = Toast.makeText(binding.root.context,"¡Muy bien hecho!",Toast.LENGTH_LONG).show()
+        transitionDrawable.startTransition(5000)
     }
-    fun equivocado(){
-        
+    private fun equivocado(){
+        val colorDrawables = arrayOf(ColorDrawable(Color.parseColor("#7f0000")),ColorDrawable(Color.WHITE))
+        val transitionDrawable = TransitionDrawable(colorDrawables)
+        binding.fondo.background = transitionDrawable
+        val mensaje = Toast.makeText(binding.root.context,"Parece que esa no es la respuesta, vuelve a intentar",Toast.LENGTH_LONG).show()
+        transitionDrawable.startTransition(1000)
     }
 }
